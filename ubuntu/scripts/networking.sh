947e28479da9206cb655201b8fb1cf8e3782da85
Ilker Ispir <ilker@ilkerispir.com>
2022-05-25 20:21:48 +0300

    docs: modified folder arch

--- ubuntu/scripts/networking.sh
+++ ubuntu/scripts/networking.sh
@@ -4,12 +4,12 @@ ubuntu_version="`lsb_release -r | awk '{print $2}'`";
 major_version="`echo $ubuntu_version | awk -F. '{print $1}'`";
 
 if [ "$major_version" -ge "18" ]; then
-echo "Create netplan config for enp0s8"
+echo "Create netplan config for eth0"
 cat <<EOF >/etc/netplan/01-netcfg.yaml;
 network:
   version: 2
   ethernets:
-    enp0s8:
+    eth0:
       dhcp4: true
 EOF
 else
--- ubuntu/scripts/vagrant.sh
+++ ubuntu/scripts/vagrant.sh
@@ -1,11 +1,7 @@
 #!/bin/bash -eux
 
 pubkey_url="https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub";
-# set a default HOME_DIR environment variable if not set
-HOME_DIR="${HOME_DIR:-/home/vagrant}";
 mkdir -p $HOME_DIR/.ssh;
-chmod 700 $HOME_DIR/.ssh;
-
 if command -v wget >/dev/null 2>&1; then
     wget --no-check-certificate "$pubkey_url" -O $HOME_DIR/.ssh/authorized_keys;
 elif command -v curl >/dev/null 2>&1; then
@@ -14,10 +10,5 @@ else
     echo "Cannot download vagrant public key";
     exit 1;
 fi
-
-chown -R vagrant:vagrant $HOME_DIR/.ssh;
-chmod 600 $HOME_DIR/.ssh/authorized_keys;
-# chmod -R go-rwsx $HOME_DIR/.ssh;
-
-echo "Get authorized_keys...";
-cat $HOME_DIR/.ssh/authorized_keys;
+chown -R vagrant $HOME_DIR/.ssh;
+chmod -R go-rwsx $HOME_DIR/.ssh;

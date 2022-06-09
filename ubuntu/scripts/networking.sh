a41acb3561f2b1ac9eabde4a0391f7a0a3589006
ilkerispir <ilker@ilkerispir.com>
2022-06-09 15:13:29 +0300

    fix: modified network.sh

--- ubuntu/scripts/networking.sh
+++ ubuntu/scripts/networking.sh
@@ -16,8 +16,3 @@ else
   # Adding a 2 sec delay to the interface up, to make the dhclient happy
   echo "pre-up sleep 2" >> /etc/network/interfaces;
 fi
-
-# Disable Predictable Network Interface names and use eth0
-[ -e /etc/network/interfaces ] && sed -i 's/en[[:alnum:]]*/eth0/g' /etc/network/interfaces;
-sed -i 's/GRUB_CMDLINE_LINUX="\(.*\)"/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0 \1"/g' /etc/default/grub;
-update-grub;
